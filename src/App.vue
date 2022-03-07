2aed0102c41070677196d442244859c3f1c4b141
Liana_Pers <lianam881@gmail.com>
2022-03-07 23:43:49 +0500

    save 1

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <div id="app">
     <div class="container">
           <!-- @что-то используется, что отправить полученные данные в какую-либо функцию, в ланном случае начения инпутов в функции савньютаск -->
     <GetTasks @add-task="saveNewTask" />
@@ -8,15 +7,13 @@
       <p class="title">Список задач</p>
   <PanelTasks v-for="(task, index) in tasks" :key="index" :task="task"/>
     </div>
-   
     </div>
-
-  </div>
 </template>
 
 <script>
 import GetTasks from "./components/GetTasks.vue";
 import PanelTasks from "./components/PanelTasks.vue"
+
 export default {
   name: "App",
   data() {
@@ -45,54 +42,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-*,
-*::after,
-*::before {
-  box-sizing: inherit;
-}
-
-body{
-    background-color: rgb(48, 20, 5);
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 16px;
-}
-
-.flex {
-  display: flex;
-  justify-content: space-around;
-  flex-wrap: wrap;
-}
-
-.container {
-  width: 1140px;
-  margin: 0 auto;
-  padding: 20px 0;
-}
-
-.title {
-  margin: 0;
-  align-self: center;
-  font-weight: bold;
-  font-size: 36px;
-  margin-bottom: 30px;
-  color:beige;
-}
-
-.output-form {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  width: 100%;
-  background-color: beige;
-  margin-bottom: 10px;
-  border-radius: 10px;
-  text-align: center;
-}
-.output-form:last-child {
-  margin-bottom: 0;
-}
-</style>
-
-<style>
-
 </style>
--- src/components/PanelTasks.vue
+++ src/components/PanelTasks.vue
@@ -1,15 +1,23 @@
 <template>
-  <div id="tasks" class="output-form">
+  <div id="tasks" class="output-form flex">
         <p class="out-text">{{task.task}}</p>
         <p class="out-text">{{task.desc}}</p>
+<div class="menu-task">
+<img :src="image" class="basket" alt="delete">
+</div>
   </div>
 </template>
 
 <script>
+import image from "../image/basket.png"
 export default {
   name: "PanelTask",
-  props:  ["task"]
-  
+  props:  ["task"],
+  data() {
+    return {
+      image
+    };
+  } 
 };
 </script>
 
